569d161e0c362f29e25f15094be0474bd1fd3a11
Jordan Poles <jpoles1@gmail.com>
2016-01-17 02:59:02 -0600

    Fixed descriptions. Added screenshots.

--- src/PoopLogger.c
+++ src/PoopLogger.c
@@ -3,11 +3,11 @@
 #include "SendWindow.h"
 //App Config
 const char *bristol_scale[7] = {
-  "Separate hard lumps, like nuts (hard to pass)",
+  "Separate hard lumps, like nuts, hard to pass",
   "Sausage-shaped, but lumpy",
-  "Like a sausage but with cracks on its surface",
+  "Like a sausage but with cracks on surface",
   "Like a sausage or snake, smooth and soft",
-  "Soft blobs with clear cut edges (passed easily)",
+  "Soft blobs with clear cut edges, passed easily",
   "Fluffy pieces with ragged edges, a mushy stool",
   "Watery, no solid pieces, entirely liquid"
 };
