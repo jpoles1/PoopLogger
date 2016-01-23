6fec262cc4ab4148e51325b15a31dc253091af59
Jordan Poles <jpoles1@gmail.com>
2016-01-22 23:00:51 -0600

    Added user functionality

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,58 +1,8 @@
 var ajax = {};
-ajax.x = function() {
-    if (typeof XMLHttpRequest !== 'undefined') {
-        return new XMLHttpRequest();
-    }
-    var versions = [
-        "MSXML2.XmlHttp.6.0",
-        "MSXML2.XmlHttp.5.0",
-        "MSXML2.XmlHttp.4.0",
-        "MSXML2.XmlHttp.3.0",
-        "MSXML2.XmlHttp.2.0",
-        "Microsoft.XmlHttp"
-    ];
+ajax.x=function(){if("undefined"!=typeof XMLHttpRequest)return new XMLHttpRequest;for(var e,t=["MSXML2.XmlHttp.6.0","MSXML2.XmlHttp.5.0","MSXML2.XmlHttp.4.0","MSXML2.XmlHttp.3.0","MSXML2.XmlHttp.2.0","Microsoft.XmlHttp"],n=0;n<t.length;n++)try{e=new ActiveXObject(t[n]);break}catch(o){}return e},ajax.send=function(e,t,n,o,a){var r=ajax.x();r.open(n,e,a),r.onreadystatechange=function(){4==r.readyState&&t(r.responseText)},"POST"==n&&r.setRequestHeader("Content-type","application/x-www-form-urlencoded"),r.send(o)},ajax.get=function(e,t,n,o){var a=[];for(var r in t)a.push(encodeURIComponent(r)+"="+encodeURIComponent(t[r]));ajax.send(e+(a.length?"?"+a.join("&"):""),n,"GET",null,o)},ajax.post=function(e,t,n,o){var a=[];for(var r in t)a.push(encodeURIComponent(r)+"="+encodeURIComponent(t[r]));ajax.send(e,n,"POST",a.join("&"),o)};
 
-    var xhr;
-    for(var i = 0; i < versions.length; i++) {
-        try {
-            xhr = new ActiveXObject(versions[i]);
-            break;
-        } catch (e) {
-        }
-    }
-    return xhr;
-};
-
-ajax.send = function(url, callback, method, data, sync) {
-    var x = ajax.x();
-    x.open(method, url, sync);
-    x.onreadystatechange = function() {
-        if (x.readyState == 4) {
-            callback(x.responseText)
-        }
-    };
-    if (method == 'POST') {
-        x.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    }
-    x.send(data)
-};
-
-ajax.get = function(url, data, callback, sync) {
-    var query = [];
-    for (var key in data) {
-        query.push(encodeURIComponent(key) + '=' + encodeURIComponent(data[key]));
-    }
-    ajax.send(url + (query.length ? '?' + query.join('&') : ''), callback, 'GET', null, sync)
-};
-
-ajax.post = function(url, data, callback, sync) {
-    var query = [];
-    for (var key in data) {
-        query.push(encodeURIComponent(key) + '=' + encodeURIComponent(data[key]));
-    }
-    ajax.send(url, callback, 'POST', query.join('&'), sync)
-};
-
+var url = "http://192.168.1.109:8000"
+//var url = "http://poopguru.herokuapp.com";
 Pebble.addEventListener('ready', function(e) {
   console.log('JavaScript app ready and running!');
   if(Pebble.getActiveWatchInfo) {
@@ -65,8 +15,14 @@ Pebble.addEventListener("appmessage", function(data){
   bristol = data.payload[5];
   console.log("Bristol val is:", bristol)
   var http = new XMLHttpRequest();
-  var url = "http://192.168.1.109:8000/tracker"
-  var url = "http://poopguru.herokuapp.com/tracker";
   var d = new Date();
-  ajax.get(url, {"bristol": bristol, "hour": d.getHours(), "dow": d.getDay()}, function(){console.log("SENT")});
+  var idvar = Pebble.getAccountToken();
+  if(localStorage.getItem("uname")){
+    idvar=localStorage.getItem("uname");
+  }
+  ajax.get(url+"/tracker", {"bristol": bristol, "hour": d.getHours(), "dow": d.getDay(), "id": idvar}, function(){console.log("SENT")});
+});
+Pebble.addEventListener('showConfiguration', function(e) {
+  // Show config page
+  Pebble.openURL(url+"/newUserForm");
 });
