8c1a07f4401f2f33c7b6bb9c5fe52d6236a1f8b4
Jordan Poles <jpoles1@gmail.com>
2016-01-22 23:02:35 -0600

    fixed url

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -2,7 +2,7 @@ var ajax = {};
 ajax.x=function(){if("undefined"!=typeof XMLHttpRequest)return new XMLHttpRequest;for(var e,t=["MSXML2.XmlHttp.6.0","MSXML2.XmlHttp.5.0","MSXML2.XmlHttp.4.0","MSXML2.XmlHttp.3.0","MSXML2.XmlHttp.2.0","Microsoft.XmlHttp"],n=0;n<t.length;n++)try{e=new ActiveXObject(t[n]);break}catch(o){}return e},ajax.send=function(e,t,n,o,a){var r=ajax.x();r.open(n,e,a),r.onreadystatechange=function(){4==r.readyState&&t(r.responseText)},"POST"==n&&r.setRequestHeader("Content-type","application/x-www-form-urlencoded"),r.send(o)},ajax.get=function(e,t,n,o){var a=[];for(var r in t)a.push(encodeURIComponent(r)+"="+encodeURIComponent(t[r]));ajax.send(e+(a.length?"?"+a.join("&"):""),n,"GET",null,o)},ajax.post=function(e,t,n,o){var a=[];for(var r in t)a.push(encodeURIComponent(r)+"="+encodeURIComponent(t[r]));ajax.send(e,n,"POST",a.join("&"),o)};
 
 var url = "http://192.168.1.109:8000"
-//var url = "http://poopguru.herokuapp.com";
+var url = "http://poopguru.herokuapp.com";
 Pebble.addEventListener('ready', function(e) {
   console.log('JavaScript app ready and running!');
   if(Pebble.getActiveWatchInfo) {
